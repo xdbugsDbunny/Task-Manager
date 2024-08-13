23da28298227a56deb99e3d90ce1158bad950289
Suraj Singh Negi <104298735+xdbugsDbunny@users.noreply.github.com>
2024-08-14 01:29:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "other-header"],
   })
