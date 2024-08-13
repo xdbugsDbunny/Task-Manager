328e40adbb1c11c95a218e37c2c252c0b8f9320e
Suraj Singh Negi <104298735+xdbugsDbunny@users.noreply.github.com>
2024-08-14 03:09:32 +0530

    multer fix fir vercel

--- api/middlewares/multer.middleware.js
+++ api/middlewares/multer.middleware.js
@@ -1,8 +1,9 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
--- api/utils/cloudinary.js
+++ api/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import path from "path";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -10,15 +11,18 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
+    
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    fs.unlinkSync(localFilePath);
-    // console.log("File Uploaded", response);
+
+    fs.unlinkSync(localFilePath); // Remove the temporary file after upload
 
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally save temp file as the upload operation got failed
+    fs.unlinkSync(localFilePath); // Clean up the temporary file on error
+    console.error("Cloudinary upload failed:", error);
+    return null;
   }
 };
 
